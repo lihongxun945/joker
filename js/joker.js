e3138caffea3fe645ff9afbe8b0af9f6b9e7cb43
= <=>
2011-10-23 21:21:05 +0800

    add joker.dom.g()

--- js/joker.js
+++ js/joker.js
@@ -17,11 +17,13 @@ joker.dom={};
 /**
 *根据classname取得元素
 *@param    {string} strClassName：要查找的className
-*@param    {object} objElm:目标元素，取目标元素的子元素
+*@param#optional    {object} objElm:目标元素，取目标元素的子元素
 *@return   {array}
+*@refer http://www.cnblogs.com/rubylouvre/archive/2009/07/24/1529640.html
 */
 joker.dom.getElementsByClassName=function(strClassName,objElm){
-    var arrElements = (objElm == undefined)?document.all:objElm.all;
+    var arrElements = (objElm == undefined)?(document.all || document.getElementsByTagName("*")):(objElm.all || objElm.getElementsByTagName("*"));
+    //只有ie支持all，非ie用getElementsByTagName("*")
     var arrResults = new Array();
     var objElement;
     for(var i=0; i < arrElements.length; i++){
@@ -34,24 +36,93 @@ joker.dom.getElementsByClassName=function(strClassName,objElm){
 }
 
 /**
+*根据tagName取得元素,返回值是数组（原生的js返回值是nodeList)
+*@param    {string} strTagName：要查找的TagName
+*@param#optional    {object} objElm:目标元素，取目标元素的子元素
+*@return   {array}
+*/
+joker.dom.getElementsByTagName=function(strTagName,objElm){
+	objElm = objElm || document;
+	var arrResults = new Array();
+	var nodes = objElm.getElementsByTagName(strTagName);
+	for(var i = 0; i < nodes.length; i++){
+		arrResults.push(nodes[i]);
+	}
+	return (arrResults);
+}
+
+/**
 *	根据id获取元素
-*@param	{string}
-*@param	{object} objElm:目标元素，取目标元素的子元素
+*@param	{string} id
+*@param#optional	{object} objElm:目标元素，取目标元素的子元素
 *@return {object} 
 */
 joker.dom.getElementById=function(strId,objElm){
-	if(objElm == undefined) objElm = document;
+	objElm = objElm || document;
 	return objElm.getElementById(strId);
 }
 
 /**
-* 获取元素的通用方法
-*
+* 获取元素的通用方法,支持类似css2中的选择器：元素选择器，类选择器，id选择器，后代选择器
+*@param {string} strSelector :选择器表达式
+*@param#optional {obj} objElm:目标元素
+*@return {array}
+*@author axun;
+*@time 2011.10.23;
+*@example joker.dom.g(".header #title a"); joker.dom.g("body.container div#utity a");
 */
-joker.dom.g=function(){
-	
-}
 
+joker.dom.g=function(strSelector,objElm){
+	var arrElements = new Array();
+	arrElements.push(objElm || document);
+	var tokens = strSelector.split(" ");
+	var tempResults;	//存储查找的中间结果
+	var token;
+	var element;
+	for(var i=0;i<tokens.length;i++){
+		tempResults = new Array();
+		token = tokens[i];
+		for(var j=0;j<arrElements.length;j++){
+			element = arrElements[j];
+			if(token.charAt(0) == '#') {	//id
+				tempResults.push(joker.dom.getElementById(token.substr(1),element));
+			}
+			else if(token.charAt(0) == '.') {
+				tempResults = tempResults.concat(joker.dom.getElementsByClassName(token.substr(1),element));
+			}
+			else{	//对于元素选择器，可能存在div.classname或者div#id,或者div.classname1.classname2#id这样的情况
+				//先对#和.进行分割
+				token = token.replace(/\./g,' \.');
+				token = token.replace(/\#/g,' \#');
+				token = token.split(" ");
+				tempResults = tempResults.concat(joker.dom.getElementsByTagName(token[0],element));	//注意，不要用document.getElementsByTagName方法，因为它返回的是nodeList对象，而不是数组,不支持数组方法。
+				if(token.length>1){	//然后把结果中不符合id和classname的值删除
+					for(var k=1;k<token.length;k++){
+						t = token[k];
+						if(t.charAt(0) == '#'){
+							for(var a=0;a<tempResults.length;a++){
+								if(tempResults[a].id != t.substr(1)){
+									tempResults.splice(a,1);
+									a--;	//注意，在遍历数组并删除时，当删除了一个元素后要index--，否则会漏掉对当前index元素的判断。不要犯这个错误。
+								}
+							}
+						}
+						if(t.charAt(0) == '.'){
+							for(var a=0;a<tempResults.length;a++){
+								if(tempResults[a].className != t.substr(1)){
+									tempResults.splice(a,1);
+									a--;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		arrElements = tempResults;
+	}
+	return arrElements;
+}
 
 /**
 *输入输出流
